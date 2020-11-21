fccc3ac4f33a27b21cabb38df1ddf7ba30cb4406
Caleb L. Power <cpower@uco.edu>
2020-11-21 16:43:28 -0600

    Feature/license update (#30)
    * updated license to Apache 2.0 in accordance with the wishes of the Council.
    * fixed NOTICE
    * updated README to reflect license change

--- src/main/java/edu/uco/cs/v2c/dispatcher/V2CDispatcher.java
+++ src/main/java/edu/uco/cs/v2c/dispatcher/V2CDispatcher.java
@@ -1,13 +1,16 @@
 /*
- * Copyright (c) 2020 V2C Development Team. All rights reserved.
- * Licensed under the Version 0.0.1 of the V2C License (the "License").
- * You may not use this file except in compliance with the License.
- * You may obtain a copy of the License at <https://tinyurl.com/v2c-license>.
+ * Copyright (c) 2020 Caleb L. Power, Everistus Akpabio, Rashed Alrashed,
+ * Nicholas Clemmons, Jonathan Craig, James Cole Riggall, and Glen Mathew.
+ * All rights reserved. Licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions 
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package edu.uco.cs.v2c.dispatcher;
